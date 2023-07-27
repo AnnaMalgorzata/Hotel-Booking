f657e62d34c5a2aad0fce3bb2974922b297c1359
Anna Godek <anna.m.godek@gmail.com>
2023-07-27 21:26:24 +0200

    Add repositories to the dependency container

--- HotelBooking/HotelBooking.DataAccessLayer/DataAccessLayerServiceCollectionExtension.cs
+++ HotelBooking/HotelBooking.DataAccessLayer/DataAccessLayerServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
 ﻿using HotelBooking.DataAccessLayer.Database;
+using HotelBooking.DataAccessLayer.Repositories;
+using HotelBooking.DataAccessLayer.Repositories.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HotelBooking.DataAccessLayer;
@@ -8,6 +10,10 @@ public static class DataAccessLayerServiceCollectionExtension
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
     {
         services.AddDbContext<HotelContext>();
+
+        services.AddScoped<IGuestRepository, GuestRepository>();
+        services.AddScoped<IReservationRepository, ReservationRepository>();
+        services.AddScoped<IRoomRepository, RoomRepository>();
 
         return services;
     }
